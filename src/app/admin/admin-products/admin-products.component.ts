6c85851886d141637118d16df68b5c648e17d5ab
Shirley <xiayingfei330@gmail.com>
2018-05-29 13:06:53 +1000

    Finish: search products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ProductService } from '../../product.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent{
-  product$;
+export class AdminProductsComponent implements OnDestroy {
+  products: Product[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.product$ = this.productService.getAll();
-   }
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
+  }
 
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(product => product.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
