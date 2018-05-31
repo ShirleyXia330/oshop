e4e7d99a22cfc52488843201e975ba4f4fc6e816
Shirley <xiayingfei330@gmail.com>
2018-05-31 22:30:56 +1000

    Finish: Adding a product to the shopping cart. Find the second error: if there is no shoppingCart in database, the category could show on the left side of the page.

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../models/product';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { retry } from 'rxjs/operator/retry';
 
 @Component({
   selector: 'product-card',
@@ -9,7 +11,17 @@ import { Product } from '../models/product';
 export class ProductCardComponent {
   @Input('product') product: Product;
   @Input('show-actions') showActions = true;
+  @Input('shopping-cart') shoppingCart;
 
-  constructor() { }
+  constructor(private cartService: ShoppingCartService) { }
 
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+  }
+
+  getQuantity() {
+    if (!this.shoppingCart) return 0;
+    let item = this.shoppingCart.items[this.product.$key];
+    return item ? item.quantity : 0;
+  }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../product.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
 import 'rxjs/add/operator/switchMap';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy{
   products: Product[] = [];
   filteredProducts: Product[] = [];
   category: string;
+  cart: any;
+  subscription: Subscription;
 
   constructor(
     productService: ProductService,
-    route: ActivatedRoute) {
+    route: ActivatedRoute,
+    private shoppingCartService: ShoppingCartService) {
     productService.getAll()
     .switchMap(products => {
       this.products = products;
@@ -30,4 +35,12 @@ export class ProductsComponent {
     });
   }
 
+  async ngOnInit() {
+    this.subscription = (await this.shoppingCartService.getCart())
+      .subscribe(cart => this.cart = cart);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
