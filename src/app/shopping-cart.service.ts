cfec0095d32fa137da24ad3442df8fdd9ed1c02b
Shirley <xiayingfei330@gmail.com>
2018-06-02 01:32:03 +1000

    Finish: Displaying the number of items in the shopping cart.

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import { Product } from './models/product';
+import { ShoppingCart } from './models/shopping-cart';
 import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ShoppingCartService {
@@ -14,9 +17,9 @@ export class ShoppingCartService {
     });
   }
 
-  async getCart() {
+  async getCart() : Promise<Observable<ShoppingCart>> {
     let cartId = await this.getOrCreateCartId();
-    return this.db.object('/shopping-carts/' + cartId);
+    return this.db.object('/shopping-carts/' + cartId).map(x => new ShoppingCart(x.items));
   }
 
   private getItem(cartId: string, productId: string){
@@ -37,11 +40,19 @@ export class ShoppingCartService {
     //   });
   }
 
-  async addToCart(product: Product) {
+  private async updateItemQuantity(product: Product, change: number){
     let cartId = await this.getOrCreateCartId();
     let item$ = this.getItem(cartId, product.$key);
     item$.take(1).subscribe(item => {
-      item$.update({product: product, quantity: (item.quantity || 0) + 1});
+      item$.update({product: product, quantity: (item.quantity || 0) + change});
     });
+  }
+
+  async addToCart(product: Product) {
+    this.updateItemQuantity(product,1);
+  }
+
+  async removeFromCart(product: Product) {
+    this.updateItemQuantity(product,-1);
   }
 }
