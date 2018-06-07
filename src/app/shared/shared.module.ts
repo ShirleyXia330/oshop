47219eb0f4d85c15aba14a884dab525d4ee12330
Shirley <xiayingfei330@gmail.com>
2018-06-07 16:26:44 +1000

    Finish: Modularization

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,18 +9,37 @@ import { OrderService } from 'shared/services/order.service';
 import { ProductService } from 'shared/services/product.service';
 import { ShoppingCartService } from 'shared/services/shopping-cart.service';
 import { UserService } from 'shared/services/user.service';
+import { FormsModule } from '@angular/forms';
+import { CustomFormsModule } from 'ng2-validation';
+import { DataTableModule } from 'angular5-data-table';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    CustomFormsModule,
+    DataTableModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    NgbModule.forRoot(),
   ],
   declarations: [
     ProductCardComponent,
     ProductQuantityComponent,
   ],
   exports: [
+    CommonModule,
     ProductCardComponent,
     ProductQuantityComponent,
+    FormsModule,
+    CustomFormsModule,
+    DataTableModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    NgbModule.forRoot().ngModule,
   ],
   providers: [
     AuthService,
---
+++ src/app/shopping/shopping.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AuthGuard } from 'shared/services/auth-guard.service';
+import { SharedModule } from 'shared/shared.module';
+import { ProductsComponent } from './components/products/products.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { OrderSuccessComponent } from './components/order-success/order-success.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { ProductFilterComponent } from './components/products/product-filter/product-filter.component';
+import { ShpoppingCartSummaryComponent } from './components/shpopping-cart-summary/shpopping-cart-summary.component';
+import { ShippingFormComponent } from './components/shipping-form/shipping-form.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductsComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard] },
+      { path: 'order-success/:id', component: OrderSuccessComponent, canActivate: [AuthGuard] },
+      { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] }, // path:'my-orders' works as well, if routerLink='' in html also use 'my-orders'.
+    ])
+  ],
+  declarations: [
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    ProductFilterComponent,
+    ShpoppingCartSummaryComponent,
+    ShippingFormComponent
+  ]
+})
+export class ShoppingModule { }
