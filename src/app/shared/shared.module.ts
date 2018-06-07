db806cc775d77ef73c0b117be98a70a0be283a19
Shirley <xiayingfei330@gmail.com>
2018-06-08 01:16:28 +1000

    Improving & Fix the problem about angular5-data-table

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ProductCardComponent } from 'shared/components/product-card/product-card.component';
-import { ProductQuantityComponent } from 'shared/components/product-quantity/product-quantity.component';
-import { AuthGuard } from 'shared/services/auth-guard.service';
-import { AuthService } from 'shared/services/auth.service';
-import { CategoryService } from 'shared/services/category.service';
-import { OrderService } from 'shared/services/order.service';
-import { ProductService } from 'shared/services/product.service';
-import { ShoppingCartService } from 'shared/services/shopping-cart.service';
-import { UserService } from 'shared/services/user.service';
 import { FormsModule } from '@angular/forms';
-import { CustomFormsModule } from 'ng2-validation';
-import { DataTableModule } from 'angular5-data-table';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DataTableModule } from 'angular5-data-table';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { CustomFormsModule } from 'ng2-validation';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductQuantityComponent } from './components/product-quantity/product-quantity.component';
+import { AuthGuard } from './services/auth-guard.service';
+import { AuthService } from './services/auth.service';
+import { CategoryService } from './services/category.service';
+import { OrderService } from './services/order.service';
+import { ProductService } from './services/product.service';
+import { ShoppingCartService } from './services/shopping-cart.service';
+import { UserService } from './services/user.service';
 
 @NgModule({
   imports: [
